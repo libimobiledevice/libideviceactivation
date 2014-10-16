10df253a95f5be6637536fc9bc2ad38681b7fccf
Martin Szulecki <m.szulecki@libimobiledevice.org>
2014-10-16 02:01:23 +0200

    ideviceactivation: Update description of --service argument slightly

--- tools/ideviceactivation.c
+++ tools/ideviceactivation.c
@@ -42,7 +42,7 @@ static void print_usage(int argc, char **argv)
 	printf("\nThe following OPTIONS are accepted:\n");
 	printf("  -d, --debug\t\tenable communication debugging\n");
 	printf("  -u, --udid UDID\ttarget specific device by its 40-digit device UDID\n");
-	printf("  -s, --service URL\tuse signing service at URL instead of default\n");
+	printf("  -s, --service URL\tuse activation webservice at URL instead of default\n");
 	printf("  -h, --help\t\tprints usage information\n");
 	printf("\n");
 }
