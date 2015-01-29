4efd18011b4ebf6653fdd1f5aa27b42fd6fbc205
Martin Szulecki <m.szulecki@libimobiledevice.org>
2015-01-29 14:46:34 +0100

    tools: Use correct GPLv3 license for ideviceactivation

--- tools/ideviceactivation.c
+++ tools/ideviceactivation.c
@@ -1,22 +1,24 @@
-/**
- * @file ideviceactivate.h
- * @brief A command line tool to handle the activation process.
+/*
+ * ideviceactivation.c
+ * A command line tool to handle the activation process
  *
- * Copyright (c) 2011-2014 Mirell Development, All Rights Reserved.
+ * Copyright (c) 2011-2015 Mirell Development, All Rights Reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 #ifdef HAVE_CONFIG_H
