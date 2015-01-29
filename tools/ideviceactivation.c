79bbdc028f6e6e0a9015aeb9fecaaef875b9ccad
Martin Szulecki <m.szulecki@libimobiledevice.org>
2015-01-29 17:30:23 +0100

    Fix incorrect license text

--- tools/ideviceactivation.c
+++ tools/ideviceactivation.c
@@ -4,8 +4,6 @@
  *
  * Copyright (c) 2011-2015 Mirell Development, All Rights Reserved.
  *
- * Licensed under the GNU General Public License Version 2
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
