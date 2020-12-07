7900661c95b1ee82d4fc00c7755f6a3f6cc15e9f
Jacek Roszkowski <j.roszk@gmail.com>
2020-12-07 01:58:02 +0100

    Header field names are case-insensitive.
    https://tools.ietf.org/html/rfc7230#section-3.2

--- src/activation.c
+++ src/activation.c
@@ -596,7 +596,7 @@ static size_t idevice_activation_header_callback(void *data, size_t size, size_t
 			}
 		}
 		if (value) {
-			if (strcmp(header, "Content-Type") == 0) {
+			if (strncasecmp(header, "Content-Type", 12) == 0) {
 				if (strcmp(value, "text/xml") == 0) {
 					response->content_type = IDEVICE_ACTIVATION_CONTENT_TYPE_PLIST;
 				} else if (strcmp(value, "application/xml") == 0) {
